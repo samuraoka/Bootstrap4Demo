383bd1f2cb553e1ae704d5047c6e2764fbf13e34
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-12-08 05:09:54 +0900

    Using a Dropdown for a Select

--- Bootstrap4Demo/wwwroot/js/site.js
+++ Bootstrap4Demo/wwwroot/js/site.js
@@ -1,4 +1,29 @@
 ﻿$(function () {
+    "use strict";
     $("#nav-item-active").addClass("active");
 });
 
+(function () {
+    "use strict";
+
+    var $inputReason = $("#reasonInput");
+    var $pickReason = $("#pickReason");
+
+    $("#reasonDropdown a").on("click", function () {
+        var reason = $(this).text();
+        $inputReason.val(reason)
+        $pickReason.text(reason);
+    });
+
+}) ();
+
+(function () {
+    "use strict";
+
+    var $inputReason = $("#reasonInput").val();
+    if ($inputReason)
+    {
+        $("#pickReason").text($inputReason);
+    }
+
+})();
