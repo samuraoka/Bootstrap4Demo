6bbbf4d56354c84f1c2c990a69aa26b538fab75d
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-12-07 09:57:24 +0900

    translated to mvc

---
+++ Bootstrap4Demo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bootstrap4Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Bootstrap4Demo/wwwroot/js/site.js
@@ -0,0 +1,4 @@
+﻿$(function () {
+    $("#nav-item-active").addClass("active");
+});
+
